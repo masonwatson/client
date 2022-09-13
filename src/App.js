7c9b8bab6d1bd5d7554573984cb711f811f0782e
masonwatson <84687363+masonwatson@users.noreply.github.com>
2022-09-13 17:13:03 -0500

    adjusted router for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
